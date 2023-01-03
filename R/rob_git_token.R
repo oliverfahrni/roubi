f15086a78eb4432dc769cc797ff4e83b1f835934
Oubi <oubifahrni@me.com>
2023-01-03 10:28:57 +0000

    update token

--- R/rob_git_token.R
+++ R/rob_git_token.R
@@ -3,10 +3,9 @@
 #' The ouput of the function is the token to access github
 #'
 #' @return github access token
-#' @import
 #' @export
 
 
 rob_git_token <- function() {
-  return("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")
+  return("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")
 }
